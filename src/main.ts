156fd9ca86c418da67f86c0873f893321917c4e2
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-01-23 01:40:25 -0300

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from 'commons/interceptors';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const fastifyAdapter = new FastifyAdapter({
@@ -13,7 +14,27 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter);
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   app.useGlobalInterceptors(new TransformInterceptor())
-  
-  await app.listen(3000);
+
+  const swaggerDocumentBuilder = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle(process.env.SYSTEM_TITLE)
+    .setDescription(process.env.SYSTEM_DESCRIPTION)
+    .setVersion(process.env.SYSTEM_VERSION)
+    .build();
+
+  const swaggerDocumentOptions: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) =>
+      `${controllerKey}_${methodKey}`,
+  };
+
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    swaggerDocumentBuilder,
+    swaggerDocumentOptions,
+  );
+
+  SwaggerModule.setup("docs", app, swaggerDocument);
+
+  await app.listen(Number(process.env.PORT) || 3333);
 }
 bootstrap();
